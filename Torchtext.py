aff520f8216daeff7a6137a493da4c23fda88992
Jacob Johansen <hvadmednej@hotmail.com>
2018-11-19 16:26:21 +0100

    Added Mask for loss

--- Torchtext.py
+++ Torchtext.py
@@ -7,8 +7,9 @@ Created on Mon Nov 12 15:27:39 2018
 
 from torchtext import data
 import torch
+import numpy as np
 
-vocab_size = 80
+vocab_size = 800
 batch_size = 20
 epochs = 1
 path = '/home/jacob/Downloads/DeepLearning_summarization-master (2)/SampleData/train.csv'
@@ -17,7 +18,7 @@ glove_dim = 50
 
 
 TEXT = data.Field(sequential=True)
-LABEL = data.Field(sequential=False)
+LABEL = data.Field(sequential=True)
 train_set = data.TabularDataset(path, 'CSV', fields=[('data', TEXT), ('label', LABEL)], skip_header=True ) 
 TEXT.build_vocab(train_set, max_size = vocab_size, vectors="glove.6B."+str(glove_dim)+"d")
 LABEL.build_vocab(train_set)
@@ -30,6 +31,22 @@ embed = torch.nn.Embedding(len(vocab), glove_dim)
 #w = embed.weight.data.copy_(vocab.vectors)
 
 
+def label_mask(x, y):
+    """x is the output tensor from the network
+       y is the label tensor
+       returns the mask to use for computing the loss"""
+    mask = torch.ones(len(y), max(np.shape(y)[1], np.shape(x)[1])) 
+    for i in range(len(y)):
+        x_index = len(x) - 1
+        try:
+            y_index = np.where(y[i]==1)[0][0]
+            index = max(x_index, y_index)
+            mask[i,index:] = 0
+        except:
+            pass
+    return mask
+    
+
 #TODO: Why are the training examples in the columns?
 #TODO: How do we enable sort?
 
@@ -41,10 +58,10 @@ dataset_iter = data.Iterator(train_set, batch_size=batch_size, device=0,
 for epoch in range(epochs):
     for examples in dataset_iter:
         x = examples.data
-        y = examples.label
-        x = embed(x)        
+        y = torch.t(examples.label)
+        x = embed(torch.t(x))        
         print(x)
-        print(y)
+        print(y[0])
         break
  
    
