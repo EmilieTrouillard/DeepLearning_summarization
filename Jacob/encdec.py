8743c9156d25c1f57ed88837dc12e842c9b011d6
Jacob Johansen <hvadmednej@hotmail.com>
2018-12-04 13:22:10 +0100

    With attention

--- Jacob/encdec.py
+++ Jacob/encdec.py
@@ -10,30 +10,29 @@ import torch
 import numpy as np
 from torch import nn
 import torch.optim as optim
+import socket
 
-MAX_LENGTH=966#summary max length
-vocab_size = 80
-batch_size = 20
-epochs = 10
-attention_size = 2
-#path = '/home/jacob/Downloads/DeepLearning_summarization-master (2)/SampleData/train.csv'
-path = '/media/ubuntu/1TO/DTU/courses/DeepLearning/DeepLearning_summarization/SampleData/train_short.csv'
-path_val = '/media/ubuntu/1TO/DTU/courses/DeepLearning/DeepLearning_summarization/SampleData/validation_short.csv'
+MAX_LENGTH=966 #summary max length
+vocab_size = 80 #Size of the vocab
+batch_size = 20 #Batch size
+epochs = 10 #How many epochs we train
+attention_features = 2 #The number of features we calculate in the attention (Row amount of Wh, abigail eq 1)
+vocab_features = 10000 #The number of features we calculate when we calculate the vocab (abigail eq 4)
+
+if socket.gethostname() == 'jacob':
+    path = '/home/jacob/Desktop/DeepLearning_summarization/Data/train_short.csv'
+    path_val = '/home/jacob/Desktop/DeepLearning_summarization/Data/validation_short.csv'
+else:
+    path = '/media/ubuntu/1TO/DTU/courses/DeepLearning/DeepLearning_summarization/SampleData/train_short.csv'
+    path_val = '/media/ubuntu/1TO/DTU/courses/DeepLearning/DeepLearning_summarization/SampleData/validation_short.csv'
 glove_dim = 50
+
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 layers_enc=2 #Num of layers in the encoder
 layers_dec=2 #Num of layers in the decoder
 
-h_size_enc = vocab_size #Hidden size of encoder
-h_size_dec = vocab_size #Hidden size of decoder
-
-#seqlen=x.size(0)
-#embedsize=x.size(-1)
-
-
-
-decout=vocab_size
+hidden_size = 100 #Hiddensize dimension (double the size for encoder, because bidirectional)
 
 #TODO: How do we enable sort in dataloader?
 
@@ -137,18 +136,18 @@ class BiDecoderRNN(nn.Module):
     def __init__(self, input_size, hidden_size):
         #Initialize variables in the class
         super(BiDecoderRNN, self).__init__()
-        self.hidden_size = hidden_size #Should be as long as vocab?
+        self.hidden_size = hidden_size
         self.lstm = nn.LSTM(input_size=input_size, hidden_size=self.hidden_size,
                             bidirectional=False, num_layers=layers_dec)
         
         self.reduce_dim = nn.Linear(2*hidden_size, hidden_size, bias=True)
-        self.linearOut = nn.Linear(hidden_size, vocab_size) #TODO: bias?
-        self.attention = nn.Linear(2*self.hidden_size + self.hidden_size, attention_size, bias=True)
+        self.linearOut = nn.Linear(hidden_size, vocab_size) #Not used when we apply attention
+        self.attention = nn.Linear(2*self.hidden_size + self.hidden_size, attention_features, bias=True)
         self.tanh = nn.Tanh()
-        self.attn_out = nn.Linear(attention_size, 1, bias=False)
+        self.attn_out = nn.Linear(attention_features, 1, bias=False)
         self.softmax = nn.Softmax(dim=0)
-        self.linearVocab1 = nn.Linear(1,1, bias=True) #TODO; fix dimension size
-        self.linearVocab2 = nn.Linear(1, vocab_size, bias=True) #TODO; fix dimension size
+        self.linearVocab1 = nn.Linear(2*hidden_size+hidden_size,vocab_features, bias=True) 
+        self.linearVocab2 = nn.Linear(vocab_features, vocab_size, bias=True) 
 
     def forward(self, output_enc, input_dec, hidden_enc, cell_state_enc):
         #During training input_dec should be the label (sequence), during test it should the previous output (one word)
@@ -162,21 +161,30 @@ class BiDecoderRNN(nn.Module):
         
         output_dec, (hidden_dec, cell_state_dec) = self.lstm(input_dec, (new_enc, new_cell))
         
+        pvocab = torch.zeros((len(output_dec), batch_size, vocab_size)).cuda()      
+        
         #Attention
         #We loop over t in the equation
         for t in range(len(output_dec)):
             #Expand negates a loop over i
+            #Output_dec contains all decoder states at times t=0, ... , t=N
             input_attention = torch.cat((output_enc, output_dec[t:t+1].expand(len(output_enc), batch_size, self.hidden_size)), dim=2)
             e = self.attention(input_attention)
             e = self.tanh(e)
             e = self.attn_out(e)
             attention = self.softmax(e)
+            
             #Calculate context vector sum(a_i^t * h_i)
             #This becomes a weighted sum of hidden states, hidden states created
             #after inputing a word with high attention has more weight
             context = torch.sum(attention * output_enc, dim=0).reshape((1,batch_size, 2* self.hidden_size))
-            p_vocab = self.linearOut(output_dec)
-        return p_vocab, (hidden_dec, cell_state_dec)
+            
+            #Calculate Pvocab (no softmax in training loop as it is included in cross entropy loss)
+            p_vocab = torch.cat((output_dec[t:t+1],context),2)
+            p_vocab = self.linearVocab1(p_vocab)
+            p_vocab = self.linearVocab2(p_vocab)
+            pvocab[t] = p_vocab
+        return pvocab, (hidden_dec, cell_state_dec)
 
  #reduction='none' because we want one loss per element and then apply the mask
 def forward_pass(encoder, decoder, x, label, criterion):
@@ -200,6 +208,7 @@ def forward_pass(encoder, decoder, x, label, criterion):
     output_enc, (hidden_enc, cell_state_enc)=encoder.forward(x, hidden_enc, cell_state_enc)
     
     output, (hidden_dec, cell_state_dec) = decoder.forward(output_enc, label[:-1], hidden_enc, cell_state_enc)
+        
     
     out = output.permute([0,2,1]) #N,C,d format where C number of classes for the Cross Entropy
 
@@ -213,7 +222,6 @@ def forward_pass(encoder, decoder, x, label, criterion):
     return output, mean_loss, label_hat
 
         
-#%%
 def forward_pass_val(encoder, decoder, x):
     #Reinitialize the state to zero since we have a new sample now.
     (hidden_enc, cell_state_enc) = encoder.initHidden(train=False)
@@ -248,8 +256,8 @@ for examples in dataset_iter:
     x = embed(x1).cuda()        
     break
     
-encoder = BiEncoderRNN(glove_dim, 100).to(device)
-decoder = BiDecoderRNN(1, 100).to(device)
+encoder = BiEncoderRNN(glove_dim, hidden_size).to(device)
+decoder = BiDecoderRNN(1, hidden_size).to(device)
 
 out, loss, label_hat = forward_pass(encoder, decoder, x, y, criterion)
 #%%        
